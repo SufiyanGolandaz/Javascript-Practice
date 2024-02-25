cf340e9e5b6ad7e16ce52b4919d43efa8835edb5
Sufiyan GOLANDAZ <42806710+SufiyanGolandaz@users.noreply.github.com>
2024-02-25 15:36:15 +0530

    Math

---
+++ Basics/07_Math.js
@@ -0,0 +1,10 @@
+console.log(Math);
+console.log(Math.abs(-4.2));//prints positive value
+console.log(Math.max(2,100));//100
+console.log(Math.min(2,100));//2
+
+console.log(Math.random());
+
+let min=10,max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));//randomize value betn ranges
